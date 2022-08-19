f42f3fdf924ec469554377029e5c5726138ff15c
AbramGirgis <girgis.abram@gmail.com>
2022-08-19 01:35:29 -0400

    added a package

---
+++ src/com/example/Example.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a value: ");
+        System.out.println("You entered: " + scanner.nextLine());
+
+
+    }
+}
+
