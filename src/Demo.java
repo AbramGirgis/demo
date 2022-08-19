70359d1cfcc0d29755f902de9cb99edf2003943d
AbramGirgis <girgis.abram@gmail.com>
2022-08-19 01:03:14 -0400

    using Scanner

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a value: ");
+
+        System.out.println("You entered: " + scanner.nextLine());
     }
 }
